10e16c73e2b973cce2fee54b1ca92801004f9ab8
marc pidjoma <pidjoma@outlook.fr>
2023-10-09 23:49:33 -0400

    add migrate database and context

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,13 +1,28 @@
+using CommandAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+string? connString = builder.Configuration.GetConnectionString("PostgreSqlConnection");
+
 
 //Add services to the container
+builder.Services.AddDbContext<CommandContext>(options =>
+{
+    options.UseNpgsql(connString);
+});
+
 builder.Services.AddControllers();
+
+
 
 
 var app = builder.Build();
 
 //Configure the HTTP request pipeline.
 app.MapControllers();
+
 
 app.MapGet("/", () => "Hello World!");
 

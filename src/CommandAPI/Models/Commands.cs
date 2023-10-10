776bd8c95ca36fe062bc25052a67dfc7b7d5f98d
marc pidjoma <pidjoma@outlook.fr>
2023-10-09 22:02:59 -0400

    Add DB context commandContext

--- src/CommandAPI/Models/Commands.cs
+++ src/CommandAPI/Models/Commands.cs
@@ -3,8 +3,9 @@ namespace CommandAPI.Models
     public class Command
     {
         public int Id { get; set; }
-        public string HowTo { get; set;}
-        public string Platform { get; set; }
-        public string CommandLine { get; set; }
+        public string? HowTo { get; set;}
+        public string? Platform { get; set; }
+        public string? CommandLine { get; set; }
     }
+   
 }
